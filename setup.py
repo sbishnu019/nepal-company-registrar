09e0bf2e5f5f2ed43482d7148ddfd22d84a8000f
Bishnu <sbishnu019@gmail.com>
2020-05-02 16:37:28 +0545

    v0.0.2 released

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nepal-company-registrar",
-    version="0.0.1",
+    version="0.0.2",
     author="Bishnu Sharma",
     author_email="sbishnu019@gmail.com",
     description="A package for nepal company registrar",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sbishnu019/nepal-company-registrar.git",
-    download_url='https://github.com/sbishnu019/nepal-company-registrar/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/sbishnu019/nepal-company-registrar/archive/v0.0.2.tar.gz',
     install_requires=[
           'requests',
           'beautifulsoup4',
